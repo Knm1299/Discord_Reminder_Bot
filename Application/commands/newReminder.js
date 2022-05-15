708774f44e0850a9fee35d555c49ba43ff571187
Keven Mitchell <keven.mitchell@revature.com>
2022-05-15 16:20:06 -0400

    added single reminder functionality including command

---
+++ Application/commands/newReminder.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { addReminder } = require('../Services/reminderManager');
+const configMan = require('../services/configManager');
+
+let config = configMan.config;
+const channelNames = Object.keys(config.regLinks);
+let choiceArr = new Array();
+for(let i = 0 ; i<channelNames.length; i++){
+    choiceArr[i] = [channelNames[i], channelNames[i]];
+}
+
+module.exports = {
+    data: new SlashCommandBuilder().setName('newreminder')
+    .setDescription('add a single additional reminder for the specified group')
+    .addStringOption(option=>
+        option.setName('group')
+            .setDescription('The group to configure this channel for')
+            .setChoices(choiceArr)
+            .setRequired(true))
+    .addStringOption(option=>
+        option.setName('time')
+            .setDescription('the date/time for this reminder in a javascript readable format ex 2022-05-13T15:50')
+            .setRequired(true))
+    .addStringOption(option=>
+        option.setName('contents')
+            .setDescription('the contents of the reminder to be scheduled')
+            .setRequired(true)),
+    async execute(interaction){
+        addReminder(interaction.options.data[0].value,interaction.options.data[1].value,interaction.options.data[2].value);
+        interaction.reply({content:'Reminder scheduled successfully', ephemeral:true});
+    }
+}
